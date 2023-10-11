c6b112fae88faebfb5ecf488f9e915d13bd498a8
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-10-10 22:23:33 -0300

    feat: add user model template

---
+++ src/main/java/br/com/vandersuncat/springtodolist/user/UserController.java
@@ -0,0 +1,18 @@
+package br.com.vandersuncat.springtodolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+
+  @PostMapping("/")
+  public void create(@RequestBody UserModel user) {
+    System.out.println(user.getUsername());
+  }
+
+}
