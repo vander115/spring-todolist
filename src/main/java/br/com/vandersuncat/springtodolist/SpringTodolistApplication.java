8e9d9427db7548caba4fbbf82f2ccd71740ede24
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-10-10 21:21:40 -0300

    first commit

---
+++ src/main/java/br/com/vandersuncat/springtodolist/SpringTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.vandersuncat.springtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodolistApplication.class, args);
+	}
+
+}
